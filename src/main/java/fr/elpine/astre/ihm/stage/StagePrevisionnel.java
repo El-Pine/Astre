8d3d722a2ab0ad9b1678638a3831166fff1b0d7f
Clem76 <clement.daubeuf@gmail.com>
2023-12-12 15:40:41 +0100

    Préparation IHM

---
+++ src/main/java/fr/elpine/astre/ihm/stage/StagePrevisionnel.java
@@ -0,0 +1,57 @@
+package fr.elpine.astre.ihm.stage;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StagePrevisionnel
+{
+	private Stage stage;
+
+	public static Stage creer() throws IOException
+	{
+		Stage stage = new Stage();
+
+		FXMLLoader fxmlLoader = new FXMLLoader(StagePrevisionnel.class.getResource("previsionnel.fxml"));
+
+		Scene scene = new Scene(fxmlLoader.load(), 700, 450);
+
+		((StagePrevisionnel) fxmlLoader.getController()).setStage(stage);
+
+		stage.setTitle("Previsions");
+		stage.setScene(scene);
+
+		return stage;
+	}
+
+	private void setStage(Stage stage) { this.stage = stage; }
+
+	public void onBtnCreeSAE(ActionEvent actionEvent)
+	{
+	}
+
+	public void onBtnCreeStage(ActionEvent actionEvent)
+	{
+	}
+
+	public void onBtnModifierPre(ActionEvent actionEvent)
+	{
+	}
+
+	public void onBtnModifierSuppr(ActionEvent actionEvent)
+	{
+	}
+
+	public void onBtnCreeRessource(ActionEvent actionEvent) {
+	}
+
+	public void onBtnRetour(ActionEvent actionEvent) throws IOException {
+		stage.close();
+		StagePrincipal.creer().show();
+	}
+}
