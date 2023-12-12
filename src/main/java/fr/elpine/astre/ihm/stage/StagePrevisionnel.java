b8e2eac3cf35bacdc09b156a1aecd87672b5b827
Clem76 <clement.daubeuf@gmail.com>
2023-12-12 16:32:29 +0100

    indent

--- src/main/java/fr/elpine/astre/ihm/stage/StagePrevisionnel.java
+++ src/main/java/fr/elpine/astre/ihm/stage/StagePrevisionnel.java
@@ -1,8 +1,10 @@
 package fr.elpine.astre.ihm.stage;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -34,25 +36,32 @@ public class StagePrevisionnel
 
 	private void setStage(Stage stage) { this.stage = stage; }
 
+	@FXML
 	public void onBtnCreeSAE(ActionEvent actionEvent)
 	{
+
 	}
 
+	@FXML
 	public void onBtnCreeStage(ActionEvent actionEvent)
 	{
 	}
 
+	@FXML
 	public void onBtnModifierPre(ActionEvent actionEvent)
 	{
 	}
 
+	@FXML
 	public void onBtnModifierSuppr(ActionEvent actionEvent)
 	{
 	}
 
+	@FXML
 	public void onBtnCreeRessource(ActionEvent actionEvent) {
 	}
 
+	@FXML
 	public void onBtnRetour(ActionEvent actionEvent) throws IOException {
 		stage.close();
 		StagePrincipal.creer().show();
