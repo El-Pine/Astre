91a9ff4ff1820980e0662978968d0fad65612f80
Clem76 <clement.daubeuf@gmail.com>
2023-12-12 08:53:59 +0100

    Ajout de la fenêtre d'accueil

--- src/main/java/fr/elpine/astre/HelloApplication.java
+++ src/main/java/fr/elpine/astre/HelloApplication.java
@@ -10,14 +10,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
 	@Override
 	public void start(Stage stage) throws IOException {
-		FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-		Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-		stage.setTitle("Hello!");
+		FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("accueil.fxml"));
+
+		Scene scene = new Scene(fxmlLoader.load(), 300, 200);
+
+		stage.setTitle("Accueil");
 		stage.setScene(scene);
 		stage.show();
-	}
-
-	public static void main(String[] args) {
-		launch();
 	}
 }
