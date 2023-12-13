87174cd3d573d7ce42439c3eec31c72ca904a690
timberlek78 <thomasboudeele1@gmail.com>
2023-12-13 14:21:17 +0100

    Changement dans la classe Semestre

--- src/main/java/fr/elpine/astre/metier/objet/Semestre.java
+++ src/main/java/fr/elpine/astre/metier/objet/Semestre.java
@@ -8,49 +8,16 @@ import java.util.HashMap;
 public class Semestre {
 
     private int numero;
-    private boolean estPair;
-    private HashMap<Double, ArrayList<Intervenant>> HashIntervenant;
-    private int service ;
-    private double ratioTD ;
+    private int nbHeureTotPlacer;
 
-    public Semestre(int numero, boolean estPair, HashMap<Double, ArrayList<Intervenant>> hashIntervenant, int service, double ratioTD) {
-        this.numero = numero;
-        this.estPair = estPair;
-        HashIntervenant = hashIntervenant;
-        this.service = service;
-        this.ratioTD = ratioTD;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public boolean estPair(){ return this.numero % 2 == 0;}
-
-    public HashMap<Double, ArrayList<Intervenant>> getHashIntervenant() {
-        return HashIntervenant;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public void setHashIntervenant(HashMap<Double, ArrayList<Intervenant>> hashIntervenant) {
-        HashIntervenant = hashIntervenant;
-    }
-
-    public void addIntervenant(Intervenant intervenant, double nbHeure)
+    public Semestre (int numero, int nbHeureTotPlacer)
     {
-        if(this.HashIntervenant.containsKey(nbHeure))
-        {
-            this.HashIntervenant.get(nbHeure).add(intervenant);
-        }
-        else
-        {
-            ArrayList<Intervenant> list = new ArrayList<Intervenant>();
-            list.add(intervenant);
-            this.HashIntervenant.put(nbHeure,list);
-        }
+        this.numero = numero;
+        this.nbHeureTotPlacer;
     }
+    public int getNumero          () { return numero;           }
+    public int getNbHeureTotPlacer() { return nbHeureTotPlacer; }
 
+    public void setNumero          ( int numero          ) { this.numero = numero;                     }
+    public void setNbHeureTotPlacer( int nbHeureTotPlacer) { this.nbHeureTotPlacer = nbHeureTotPlacer; }
 }
