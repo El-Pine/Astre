1029c04d5d043fa233f355c3c0dd78f5fe27eb04
timberlek78 <thomasboudeele1@gmail.com>
2023-12-14 09:56:23 +0100

    Commencement ajoutIntervenant

--- src/main/java/fr/elpine/astre/ihm/stage/StageAjoutIntervenant.java
+++ src/main/java/fr/elpine/astre/ihm/stage/StageAjoutIntervenant.java
@@ -1,8 +1,11 @@
 package fr.elpine.astre.ihm.stage;
 
+import fr.elpine.astre.Controleur;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,6 +13,13 @@ import java.io.IOException;
 public class StageAjoutIntervenant
 {
     private Stage stage;
+    private TextField txtNom;
+    private TextField txtPrenom;
+    private ComboBox  cpbContrat;
+    private TextField txtService;
+    private TextField txtComplementaire;
+    private TextField txtEmail;
+
     public static Stage creer() throws IOException
     {
         Stage stage = new Stage();
@@ -40,7 +50,13 @@ public class StageAjoutIntervenant
 
     }
 
-    public void btnValider(ActionEvent actionEvent) {
+    public void onBtnValider(ActionEvent actionEvent)
+    {
+        System.out.println(txtNom.getText());
+        System.out.println(txtPrenom.getText());
+        System.out.println(txtEmail.getText());
+        System.out.println(txtService.getText());
+        System.out.println(txtComplementaire.getText());
     }
 
     public void btnAnnuler(ActionEvent actionEvent) {
