9422aa7442c799692638a2ca6b9ec2c634f39fd0
capucine19 <capuc6ne@protonmail.com>
2023-12-14 09:54:52 +0100

    ajout getRessources controleur

--- src/main/java/fr/elpine/astre/Controleur.java
+++ src/main/java/fr/elpine/astre/Controleur.java
@@ -3,6 +3,7 @@ package fr.elpine.astre;
 import fr.elpine.astre.ihm.AstreApplication;
 import fr.elpine.astre.metier.Astre;
 import fr.elpine.astre.metier.DB;
+import fr.elpine.astre.metier.db.RessourceSql;
 import javafx.application.Application;
 
 public class Controleur
@@ -11,10 +12,12 @@ public class Controleur
 
     private DB    db;
     private Astre astre;
+    private RessourceSql ressources;
 
     private Controleur()
     {
         this.db    = new DB();
+        this.ressources = new RessourceSql();
         this.astre = new Astre( this );
 
         Application.launch(AstreApplication.class);
@@ -28,6 +31,13 @@ public class Controleur
     }
 
     public DB getDb() { return this.db; }
+
+    public void getRessources()
+    {
+        this.ressources.getRessources();
+
+    }
+
 
     public static void main(String[] args)
     {
