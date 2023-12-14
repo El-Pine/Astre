2d881dd32ae95cc09a80140724d849c519de3274
Clem76 <clement.daubeuf@gmail.com>
2023-12-14 16:30:13 +0100

    Fix du controleur

--- src/main/java/fr/elpine/astre/Controleur.java
+++ src/main/java/fr/elpine/astre/Controleur.java
@@ -5,20 +5,16 @@ import fr.elpine.astre.metier.Astre;
 import fr.elpine.astre.metier.DB;
 import javafx.application.Application;
 
-import java.util.ArrayList;
-
 public class Controleur
 {
     private static Controleur ctrl;
 
     private DB    db;
     private Astre astre;
-    private RessourceSql ressources;
 
     private Controleur()
     {
         this.db         = new DB();
-        this.ressources = new RessourceSql( this );
         this.astre      = new Astre( this );
     }
 
@@ -35,12 +31,6 @@ public class Controleur
     }
 
     public DB getDb() { return this.db; }
-
-    public ArrayList<Ressource> getRessources()
-    {
-        return this.ressources.getRessources();
-    }
-
 
     public static void main(String[] args)
     {
