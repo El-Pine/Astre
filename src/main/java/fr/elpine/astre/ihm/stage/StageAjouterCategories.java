ab8130f8c0f8b6af161764fab57c5e8c2d547561
timberlek78 <thomasboudeele1@gmail.com>
2023-12-15 13:42:04 +0100

    Activation du bouton enregistrer cat Inter et liaisions avec la DB

--- src/main/java/fr/elpine/astre/ihm/stage/StageAjouterCategories.java
+++ src/main/java/fr/elpine/astre/ihm/stage/StageAjouterCategories.java
@@ -1,8 +1,12 @@
 package fr.elpine.astre.ihm.stage;
 
+import fr.elpine.astre.Controleur;
+import fr.elpine.astre.metier.objet.CategorieIntervenant;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,7 +14,17 @@ import java.io.IOException;
 public class StageAjouterCategories
 {
     private Stage stage;
+
+    private TextField txtfCodeCatInter;
+    private TextField txtfNomCatInter;
+    private TextField txtfRatioCatInter;
+    private TextField txtfNbHMCatInter;
+    private TextField txtfNbHServCatInter;
+
+
     static StageAccueilConfig parent;
+
+
 
     public static Stage creer( StageAccueilConfig parent ) throws IOException
     {
@@ -36,9 +50,27 @@ public class StageAjouterCategories
 
     private void setStage(Stage stage) { this.stage = stage; }
 
-    public void onBtnEnregistrerCatInter(ActionEvent actionEvent) {
+    public void onBtnEnregistrerCatInter(ActionEvent actionEvent)
+    {
+        String code    =                     txtfCodeCatInter   .getText();
+        String nom     =                     txtfNomCatInter    .getText();
+        double ratioTD = Double .parseDouble(txtfRatioCatInter  .getText());
+        int    nbHM    = Integer.parseInt   (txtfNbHMCatInter   .getText());
+        int    nbHServ = Integer.parseInt   (txtfNbHServCatInter.getText());
+
+        if(estValide(code,nom))
+            Controleur.get().getDb().ajouterCategorieIntervenant(new CategorieIntervenant(code, nom,nbHM,nbHServ,ratioTD));
+
         parent.activer();
         stage.close();
+    }
+
+
+    public boolean estValide(String code, String nom)
+    {
+        if(code == null  && nom == null)
+            return false;
+        return true;
     }
 
     public void onBtnAnnulerCatInter(ActionEvent actionEvent) {
