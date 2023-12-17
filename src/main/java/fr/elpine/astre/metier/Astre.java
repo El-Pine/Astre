c59aae7504dc1432435797193d293bf2622f52cc
Clem76 <clement.daubeuf@gmail.com>
2023-12-17 16:32:22 +0100

    Classe inutile ?

--- src/main/java/fr/elpine/astre/metier/Astre.java
+++ src/main/java/fr/elpine/astre/metier/Astre.java
@@ -13,36 +13,4 @@ public class Astre
     {
         this.ctrl = ctrl;
     }
-    
-
-
-    /*
-    public void ajouterRessource(String nom, String code, String commentaire, int nbHeurePn, int nbHeurePnCours, HashMap<String, Integer> heureSemestre, int nbHeuretl)
-    {
-        Ressource ressource = new Ressource(nom,code, commentaire, nbHeurePn, nbHeurePnCours, heureSemestre, nbHeuretl);
-
-        ctrl.getDb().ajouterRessource(ressource);
-    }
-
-    public void ajouterStage(String nom, String code, String commentaire, int nbHeureREH, int nbHeureTut, int nbHeure)
-    {
-        Stage stage = new Stage (nom, code, commentaire, nbHeureREH, nbHeureTut, nbHeure);
-
-        ctrl.getDb().ajouterStage(stage);
-    }
-
-    public void ajouterSAE(String nom, String code, String commentaire, int nbHeurePnSem, int nbHeureTut, int nbHeure)
-    {
-        SAE sae = new SAE(nom, code, commentaire, nbHeurePnSem, nbHeureTut, nbHeure);
-
-        ctrl.getDb().ajouterSAE(sae);
-    }
-
-    public void ajouterCategorieIntervenant(String code, String nom, int nbHeureMax,int service, double ratioTd, boolean estCompleter)
-    {
-        CategorieIntervenant catInter = new CategorieIntervenant(code, nom, nbHeureMax,service,ratioTd,estCompleter);
-
-        ctrl.getDb().ajouterCategorieIntervenant(catInter);
-    }
-    */
 }
