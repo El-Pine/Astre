6a7ef255fcfe672bd3ec7c3bb81082f212210ae7
Empereur-Hanibal <louis.hagues@gmail.com>
2023-12-18 14:41:32 +0100

    modification StageSaisieRessource

---
+++ src/main/java/fr/elpine/astre/ihm/stage/StageAjoutRessource.java
@@ -0,0 +1,51 @@
+package fr.elpine.astre.ihm.stage;
+
+import fr.elpine.astre.Controleur;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StageAjoutRessource implements Initializable {
+    private Stage stage;
+
+    @FXML
+    private ComboBox cbb
+    public static Stage creer() throws IOException
+    {
+        Stage stage = new Stage();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(StageSaisieRessource.class.getResource("CreationModules.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 660);
+
+        StageAjoutRessource stageCtrl = fxmlLoader.getController();
+        if (stageCtrl != null) stageCtrl.setStage(stage);
+
+        stage.setTitle("Affectation");
+        stage.setScene(scene);
+
+        stage.setOnCloseRequest(e -> {
+            // perform actions before closing
+            try { StagePrevisionnel.creer().show(); } catch (IOException ignored) {}
+        });
+
+        return stage;
+    }
+
+    private void setStage(Stage stage) { this.stage = stage; }
+
+    private
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
