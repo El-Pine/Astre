b8fc8c3da78f97201487ebaac01fe92d89ae8f71
Mathys <poretmathys2004@gmail.com>
2023-12-18 10:33:31 +0100

    Réparation des commits de Thomas

---
+++ src/main/java/fr/elpine/astre/ihm/stage/StageSaisieSae.java
@@ -0,0 +1,62 @@
+package fr.elpine.astre.ihm.stage;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StageSaisieSae
+{
+    private Stage stage;
+
+    public static Stage creer() throws IOException
+    {
+        Stage stage = new Stage();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(StageSaisieSae.class.getResource("saisieSae.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 660);
+
+        StageSaisieSae stageCtrl = fxmlLoader.getController();
+        if (stageCtrl != null) stageCtrl.setStage(stage);
+
+        stage.setTitle("Saisie Sae");
+        stage.setScene(scene);
+
+        stage.setOnCloseRequest(e -> {
+            // perform actions before closing
+            try { StagePrevisionnel.creer().show(); } catch (IOException ignored) {}
+        });
+
+        return stage;
+    }
+
+    private void setStage(Stage stage) { this.stage = stage; }
+
+    @FXML
+    protected void onBtnAjouter() throws IOException {
+    }
+
+    @FXML
+    protected void onBtnSupprimer() throws IOException {
+    }
+
+    @FXML
+    protected void onBtnDetail() {
+        // A FAIRE
+    }
+
+    @FXML
+    protected void onBtnEnregistrer() throws IOException {
+        stage.close();
+        StagePrevisionnel.creer().show();
+    }
+
+    @FXML
+    protected void onBtnAnnuler() throws IOException {
+        stage.close();
+        StagePrevisionnel.creer().show();
+    }
+}
