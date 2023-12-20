1b776020d835c20a41b54312b6aaa6ae0924d9e0
Clem76 <clement.daubeuf@gmail.com>
2023-12-20 10:02:00 +0100

    Changement liste dans Astre

--- src/main/java/fr/elpine/astre/metier/Astre.java
+++ src/main/java/fr/elpine/astre/metier/Astre.java
@@ -11,7 +11,7 @@ public class Astre
     private Controleur ctrl;
     private ArrayList<Annee>    ensAnnee;
     private ArrayList<Semestre>    ensSemestre;
-    private ArrayList<Module>      ensModules;
+    private ArrayList<Module> ensModule;
     private ArrayList<Intervenant> ensIntervenant;
     private ArrayList<CategorieHeure> ensCategorieHeure;
     private ArrayList<CategorieIntervenant> ensCategorieIntervenant;
@@ -20,49 +20,14 @@ public class Astre
     public Astre ( Controleur ctrl )
     {
         this.ctrl = ctrl;
+
         this.ensAnnee                = ctrl.getDb().getAllAnnee();
         this.ensSemestre             = ctrl.getDb().getAllSemestre( this.ensAnnee );
-        this.ensModules              = ctrl.getDb().getAllModule( this.ensModules );
+        this.ensModule               = ctrl.getDb().getAllModule( this.ensSemestre );
         this.ensCategorieIntervenant = ctrl.getDb().getAllCategorieIntervenant();
         this.ensIntervenant          = ctrl.getDb().getAllIntervenant( this.ensCategorieIntervenant );
         this.ensCategorieHeure       = ctrl.getDb().getAllCategorieHeure();
-        this.ensAffectation          = ctrl.getDb().getAllAffectation( this.ensCategorieHeure, this.ensModules, this.ensIntervenant );
-        this.ensAttribution          = ctrl.getDb().getAllAttribution( this.ensCategorieHeure, this.ensModules );
-
+        this.ensAffectation          = ctrl.getDb().getAllAffectation( this.ensCategorieHeure, this.ensModule, this.ensIntervenant );
+        this.ensAttribution          = ctrl.getDb().getAllAttribution( this.ensCategorieHeure, this.ensModule);
     }
-
-
-    //Cette méthode rajoute les semestres dans une arraylist ensSemestre dans l'annee du semestre.
-    public void initListSemestreAnnee()
-    {
-        for (Annee a: ensAnnee)
-        {
-            for (Semestre s : ensSemestre)
-            {
-                if(s.getAnnee().getNom().equals(a.getNom()))
-                {
-                    a.ajouterSemestre(s);
-                    s.setAnnee(a);
-                }
-            }
-        }
-    }
-    public void initListModuleSemestre()
-    {
-        for (Semestre s : ensSemestre)
-        {
-            for (Module m : ensModules)
-            {
-                if(m.getSemestre().getNumero() == s.getNumero())
-                {
-                    s.ajouterModule(m);
-                    m.setSemestre(s);
-                }
-            }
-        }
-    }
-
-
-
-
 }
