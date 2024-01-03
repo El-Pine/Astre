7841b321bbd005b0d9f3ed8d274fdea22f995a96
Clem76 <clement.daubeuf@gmail.com>
2024-01-04 00:59:21 +0100

    Création de fraction vide

--- src/main/java/fr/elpine/astre/metier/outil/Fraction.java
+++ src/main/java/fr/elpine/astre/metier/outil/Fraction.java
@@ -19,11 +19,18 @@ public class Fraction
 		this.fraction = str.matches(REGEX_FRACTION);
 	}
 
+	private Fraction()
+	{
+		this.n = 0;
+		this.d = 0;
+		this.fraction = false;
+	}
+
 	public static Fraction valueOf( String str )
 	{
-		if (str == null || !str.matches(REGEX)) return null;
+		if (str == null || ( !str.matches(REGEX) && !str.isEmpty() )) return null;
 
-		return new Fraction( str );
+		return str.isEmpty() ? new Fraction() : new Fraction( str );
 	}
 
 	public static boolean matches( String str )
@@ -33,13 +40,14 @@ public class Fraction
 
 	public double value()
 	{
-		if (this.fraction) return (double) this.n / this.d;
-		else               return Double.parseDouble(String.format("%d.%d", this.n, this.d));
+		if (this.fraction)                   return (double) this.n / this.d;
+		else if (this.d == 0 && this.n == 0) return 0d;
+		else                                 return Double.parseDouble(String.format("%d.%d", this.n, this.d));
 	}
 
 	public String toString()
 	{
-		if (this.d == 0) return String.format( "%d", this.n );
+		if (this.d == 0) if (this.n == 0) return ""; else return String.format( "%d", this.n );
 
 		return String.format( this.fraction ? "%d/%d" : "%d.%d", this.n, this.d );
 	}
