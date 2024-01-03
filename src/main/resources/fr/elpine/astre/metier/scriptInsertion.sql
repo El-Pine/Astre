0071cae5a5aa9ee27b732dcea3701580eaf22898
Clem76 <clement.daubeuf@gmail.com>
2024-01-03 22:15:44 +0100

    mise à jour du script d'insertion

--- src/main/resources/fr/elpine/astre/metier/scriptInsertion.sql
+++ src/main/resources/fr/elpine/astre/metier/scriptInsertion.sql
@@ -20,41 +20,42 @@ INSERT INTO Semestre (numero, annee, nbGrpTD, nbGrpTP, nbEtd, nbSemaine) VALUES
 
 -- Insertion des données dans la table Module
 INSERT INTO Module (code, numeroSemestre, annee, nom, abreviation, couleur, typeModule, validation) VALUES
-    ('MATH101', 1, '2022-2023', 'Mathématiques', 'MATH', 'rgb(255,255,255)', 'CM', true),
-    ('MATH102', 1, '2022-2023', 'Analyse', 'ANA', 'rgb(255,255,255)', 'TD', true),
-    ('PHYS101', 1, '2022-2023', 'Physique', 'PHY', 'rgb(255,255,255)', 'TP', true),
-    ('MATH201', 2, '2022-2023', 'Algèbre', 'ALG', 'rgb(255,255,255)', 'CM', false),
-    ('MATH202', 2, '2022-2023', 'Géométrie', 'GEO', 'rgb(255,255,255)', 'TD', false),
-    ('PHYS201', 2, '2022-2023', 'Optique', 'OPT', 'rgb(255,255,255)', 'TP', false);
+    ('MATH101', 1, '2022-2023', 'Mathématiques', 'MATH', 'rgb(255,255,255)', 'ressource', true),
+    ('MATH102', 1, '2022-2023', 'Analyse', 'ANA', 'rgb(255,255,255)', 'ressource', true),
+    ('PHYS101', 1, '2022-2023', 'Physique', 'PHY', 'rgb(255,255,255)', 'ressource', true),
+    ('MATH201', 2, '2022-2023', 'Algèbre', 'ALG', 'rgb(255,255,255)', 'ressource', false),
+    ('MATH202', 2, '2022-2023', 'Géométrie', 'GEO', 'rgb(255,255,255)', 'ressource', false),
+    ('PHYS201', 2, '2022-2023', 'Optique', 'OPT', 'rgb(255,255,255)', 'ressource', false);
 
 -- Insertion des données dans la table CategorieHeure
 INSERT INTO CategorieHeure (nom, eqtd, ressource, sae, ppp, stage) VALUES
-    ('CM', 3/2, false, false, false, false),
-    ('TD', 1, false, false, false, false),
-    ('TP', 1, false, false, false, false),
-    ('H tut', 1, false, false, false, false),
-    ('REH', 1, false, false, false, false),
-    ('H Saé', 1, false, false, false, false),
-    ('HP', 1, false, false, false, false);
+    ('CM', '3/2', false, false, false, false),
+    ('TD', '1', false, false, false, false),
+    ('TP', '1', false, false, false, false),
+    ('H tut', '1', false, false, false, false),
+    ('REH', '1', false, false, false, false),
+    ('H Saé', '1', false, false, false, false),
+    ('HP', '1', false, false, false, false);
 
 -- Insertion des données dans la table CategorieIntervenant
 INSERT INTO CategorieIntervenant (code, nom, nbHeureMaxDefaut, nbHeureServiceDefaut, ratioTPDefaut) VALUES
-    ('CAT1', 'Enseignant', 20, 15, 0.5),
-    ('CAT2', 'Chercheur', 30, 25, 0.6);
+    ('CAT1', 'Enseignant', '20', '15', '1/2'),
+    ('CAT2', 'Chercheur', '30', '25', '3/5');
 
 -- Insertion des données dans la table Intervenant
 INSERT INTO Intervenant (nom, prenom, mail, codeCategorie, heureService, heureMax, ratioTP) VALUES
-    ('Dupont', 'Jean', 'jean.dupont@example.com', 'CAT1', 18, 20, 0.4),
-    ('Martin', 'Alice', 'alice.martin@example.com', 'CAT2', 25, 30, 0.6);
+    ('Dupont', 'Jean', 'jean.dupont@example.com', 'CAT1', '18', '20', '2/5'),
+    ('Martin', 'Alice', 'alice.martin@example.com', 'CAT2', '25', '30', '3/5');
 
 -- Insertion des données dans la table Attribution
 INSERT INTO Attribution (codeModule, numeroSemestreModule, anneeModule, nomCategorieHeure, nbHeure, nbSemaine) VALUES
-    ('MATH101', 1, '2022-2023', 'CM', 30, 12),
-    ('MATH102', 1, '2022-2023', 'TD', 15, 12),
-    ('PHYS101', 1, '2022-2023', 'TP', 20, 12);
+    ('MATH101', 1, '2022-2023', 'CM', '30', '12'),
+    ('MATH102', 1, '2022-2023', 'TD', '15', '12'),
+    ('PHYS101', 1, '2022-2023', 'TP', '20', '12');
 
 -- Insertion des données dans la table Affectation
 INSERT INTO Affectation (codeModule, numeroSemestreModule, anneeModule, idInter, typeHeure, nbGroupe, nbSemaine, nbHeure, commentaire) VALUES
-    ('MATH101', 1, '2022-2023', 1, 'CM', 1, 12, 30, 'Affectation pour le semestre 1'),
-    ('MATH102', 1, '2022-2023', 1, 'TD', 1, 12, 15, 'Affectation pour le semestre 1'),
-    ('PHYS101', 1, '2022-2023', 2, 'TP', 1, 12, 20, 'Affectation pour le semestre 1');
+    ('MATH101', 1, '2022-2023', 1, 'CM', 1, 12, null, 'Affectation pour le semestre 1'),
+    ('MATH102', 1, '2022-2023', 1, 'TD', 1, 12, null, 'Affectation pour le semestre 1'),
+    ('PHYS101', 1, '2022-2023', 2, 'TP', 1, 12, null, 'Affectation pour le semestre 1'),
+    ('PHYS101', 1, '2022-2023', 1, 'HP', null, null, 6, 'Affectation pour le semestre 1');
