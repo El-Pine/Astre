952e029708f41a61d4956bae4117475b300f6ea5
Mathys <poretmathys2004@gmail.com>
2024-01-10 13:48:40 +0100

    Alignement des textes dans les scriptes sql

--- src/main/resources/fr/elpine/astre/metier/scriptCreation.sql
+++ src/main/resources/fr/elpine/astre/metier/scriptCreation.sql
@@ -1,80 +1,86 @@
 -- Suppression des anciennes tables
 
-DROP TABLE IF EXISTS Affectation;
-DROP TABLE IF EXISTS Attribution;
-DROP TABLE IF EXISTS Intervenant;
+DROP TABLE IF EXISTS Affectation         ;
+DROP TABLE IF EXISTS Attribution         ;
+DROP TABLE IF EXISTS Intervenant         ;
 DROP TABLE IF EXISTS CategorieIntervenant;
-DROP TABLE IF EXISTS CategorieHeure;
-DROP TABLE IF EXISTS Module;
-DROP TABLE IF EXISTS Semestre;
-DROP TABLE IF EXISTS Annee;
+DROP TABLE IF EXISTS CategorieHeure      ;
+DROP TABLE IF EXISTS Module              ;
+DROP TABLE IF EXISTS Semestre            ;
+DROP TABLE IF EXISTS Annee               ;
 
 -- Création des tables
 
 CREATE TABLE Annee (
-	nom VARCHAR(255) PRIMARY KEY,
-	debut DATE,
-	fin DATE
+
+	nom     VARCHAR(255) PRIMARY KEY,
+
+	debut   DATE                    ,
+	fin     DATE
 );
 
 CREATE TABLE Semestre (
-	numero INT,
-	annee VARCHAR(255),
-	nbGrpTD INT,
-	nbGrpTP INT,
-	nbEtd INT,
-	nbSemaine INT,
+
+	numero      INT         ,
+	annee       VARCHAR(255),
+	nbGrpTD     INT         ,
+	nbGrpTP     INT         ,
+	nbEtd       INT         ,
+	nbSemaine   INT         ,
 
 	FOREIGN KEY (annee) REFERENCES Annee(nom),
-
 	PRIMARY KEY (numero, annee)
 );
 
 CREATE TABLE Module (
-	code VARCHAR(255),
-	numeroSemestre INT,
-	annee VARCHAR(255),
-	nom VARCHAR(255),
-	abreviation VARCHAR(255),
-    typeModule VARCHAR(255),
-	couleur VARCHAR(16),
-	validation BOOLEAN,
 
-	FOREIGN KEY (numeroSemestre, annee) REFERENCES Semestre(numero, annee),
+	code            VARCHAR(255),
+	numeroSemestre  INT         ,
+	annee           VARCHAR(255),
+	nom             VARCHAR(255),
+	abreviation     VARCHAR(255),
+    typeModule      VARCHAR(255),
+	couleur         VARCHAR(16) ,
+	validation      BOOLEAN     ,
 
+	FOREIGN KEY (numeroSemestre, annee      ) REFERENCES Semestre(numero, annee),
 	PRIMARY KEY (code, numeroSemestre, annee)
 );
 
 CREATE TABLE CategorieHeure (
-	nom VARCHAR(255) PRIMARY KEY,
-	eqtd TEXT,
 
-	ressource BOOLEAN,
-	sae       BOOLEAN,
-	ppp       BOOLEAN,
-	stage     BOOLEAN,
+	nom         VARCHAR(255) PRIMARY KEY,
 
-	hebdo     BOOLEAN,
-	typeGroupe VARCHAR(2)
+	eqtd        TEXT    ,
+	ressource   BOOLEAN ,
+	sae         BOOLEAN ,
+	ppp         BOOLEAN ,
+	stage       BOOLEAN ,
+	hebdo       BOOLEAN ,
+	typeGroupe  VARCHAR(2)
 );
 
 CREATE TABLE CategorieIntervenant (
-	code VARCHAR(255) PRIMARY KEY,
-	nom VARCHAR(255),
-	nbHeureMaxDefaut TEXT,
-	nbHeureServiceDefaut TEXT,
-	ratioTPDefaut TEXT
+
+	code                 VARCHAR(255) PRIMARY KEY,
+
+	nom                  VARCHAR(255),
+	nbHeureMaxDefaut     TEXT        ,
+	nbHeureServiceDefaut TEXT        ,
+	ratioTPDefaut        TEXT
 );
 
 CREATE TABLE Intervenant (
-	idInter SERIAL PRIMARY KEY,
-	nom VARCHAR(255),
-	prenom VARCHAR(255),
-	mail varchar(255),
-	codeCategorie VARCHAR(255),
-	heureService TEXT,
-	heureMax TEXT,
-	ratioTP TEXT,
+
+	idInter              SERIAL PRIMARY KEY,
+
+	nom                  VARCHAR(255),
+	prenom               VARCHAR(255),
+	mail                 varchar(255),
+	codeCategorie        VARCHAR(255),
+	heureService         TEXT        ,
+	heureMax             TEXT        ,
+	ratioTP              TEXT        ,
 
 	FOREIGN KEY (codeCategorie) REFERENCES CategorieIntervenant(code)
 );
@@ -82,13 +88,14 @@ CREATE TABLE Intervenant (
 -- Associations
 
 CREATE TABLE Attribution (
-	codeModule VARCHAR(255),
+
+	codeModule           VARCHAR(255),
 	numeroSemestreModule INT,
-	anneeModule VARCHAR(255),
-	nomCategorieHeure VARCHAR(255),
-	nbHeure TEXT,
-	nbSemaine INT,
-    nbHeurePN TEXT,
+	anneeModule          VARCHAR(255),
+	nomCategorieHeure    VARCHAR(255),
+	nbHeure              TEXT,
+	nbSemaine            INT,
+    nbHeurePN            TEXT,
 
 	FOREIGN KEY (codeModule, numeroSemestreModule, anneeModule) REFERENCES Module(code, numeroSemestre, annee),
 	FOREIGN KEY (nomCategorieHeure)                             REFERENCES CategorieHeure(nom),
@@ -97,17 +104,17 @@ CREATE TABLE Attribution (
 );
 
 CREATE TABLE Affectation (
-    idAffectation SERIAL,
-	codeModule VARCHAR(255),
-    numeroSemestreModule INT,
-    anneeModule VARCHAR(255),
-    idInter INT,
+    idAffectation        SERIAL,
+	codeModule           VARCHAR(255),
+    numeroSemestreModule INT         ,
+    anneeModule          VARCHAR(255),
+    idInter              INT         ,
 
-    typeHeure   VARCHAR(255),
-    nbGroupe    INT,
-    nbSemaine   INT,
-    nbHeure     TEXT,
-    commentaire TEXT,
+    typeHeure           VARCHAR(255) ,
+    nbGroupe            INT          ,
+    nbSemaine           INT          ,
+    nbHeure             TEXT         ,
+    commentaire         TEXT         ,
 
 	FOREIGN KEY (codeModule, numeroSemestreModule, anneeModule) REFERENCES Module(code, numeroSemestre, annee),
 	FOREIGN KEY (typeHeure)                                     REFERENCES CategorieHeure(nom),
